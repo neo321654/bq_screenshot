46c2dfa32d87b0aad75d1930171d87f84eb353e1
neo321654 <neo321654@rambler.ru>
2024-09-18 22:08:36 +0300

    last up11222

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:hotkey_manager/hotkey_manager.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  final DateTime endDate = DateTime(2024, 9, 25);
-
-  // Получаем текущую дату
-  final DateTime currentDate = DateTime.now();
-
-  // Проверяем, если текущая дата больше или равна конечной дате
-  if (currentDate.isAfter(endDate)) {
-    print('Функция завершает выполнение, так как текущая дата превышает 25 сентября 2024.');
-    return; // Выход из функции
-  }
+  // final DateTime endDate = DateTime(2024, 9, 25);
+  //
+  // // Получаем текущую дату
+  // final DateTime currentDate = DateTime.now();
+  //
+  // // Проверяем, если текущая дата больше или равна конечной дате
+  // if (currentDate.isAfter(endDate)) {
+  //   print('Функция завершает выполнение, так как текущая дата превышает 25 сентября 2024.');
+  //   return; // Выход из функции
+  // }
   // Must add this line.
   WidgetsFlutterBinding.ensureInitialized();
   // For hot reload, `unregisterAll()` needs to be called.
